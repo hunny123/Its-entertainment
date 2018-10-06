ef882e04cced826350a3628a455758847f26fc2e
Hunny <devinhunny04021997@gmail.com>
2018-10-07 01:07:51 +0530

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,8 @@
       $("#c2 .carousel-inner").append(`<div class='carousel-item ${k}' ><div>
      <iframe class="iframe-size " src="https://www.youtube.com/embed/${b.items[i].id.videoId}",allow="autoplay; encrypted-media" frameborder='0' allowFullScreen width="100%" height="300px"></div></div>`)
         $("#c2 .carousel-indicators").append(`<li data-target="#c2" data-slide-to=${i} class=${k}></li>`)
+        $("#qmuisc .carousel-inner").append(`<div class='carousel-item ${k} animated fadeIn'><div class="row pb-4 mb-2 mt-2"><div class="col-md-6 video-area ">
+     <iframe class="iframe-size" src="https://www.youtube.com/embed/${b.items[i].id.videoId}"frameborder='' allowFullScreen='' width="105%" height="300px"></iframe></div><div class="col-md-6 p-2 "><center><a href='http://api.youtube6download.top/fetch/link.php?i=${b.items[i].id.videoId}' class="btn  pt-3 ml-2 download-buttons" target="blank"><i class="fa fa-download pr-1"></i> Download Mp3</a><a href="https://lolyoutube.com/download/mp4/${b.items[i].id.videoId}/${Date.now()}"class="btn pt-3 ml-2 download-buttons" target="blank"><i class="fa fa-download  pr-1"></i> Download Mp4</a></center></div></div></div>`)
     }
     })
     $.getJSON("https://www.googleapis.com/youtube/v3/search?q=latest game trailer&maxResults=5&type=horror&key=AIzaSyCMlR6u_1n1nvEXH7rw6BpLhS_Rb7Agv8k&part=snippet", function(b) {
