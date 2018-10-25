3b7f29dec1fdb265090f27f0623f65011e880491
Hunny <devinhunny04021997@gmail.com>
2018-10-26 04:02:46 +0530

    Add files via upload

--- scripts/video.js
+++ scripts/video.js
@@ -2,17 +2,17 @@
 
 $(document).ready(function(){
 
- $.getJSON("https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLvFYFNbi-IBFeP5ALr50hoOmKiYRMvzUq&part=snippet&maxResults=10&key=AIzaSyCMlR6u_1n1nvEXH7rw6BpLhS_Rb7Agv8k", function(b) {
+ $.getJSON("https://www.googleapis.com/youtube/v3/search?q=latest+seasons+news&sp=EgIIAg%253D%253D&maxResults=10&type=video&key=AIzaSyCMlR6u_1n1nvEXH7rw6BpLhS_Rb7Agv8k&part=snippet", function(b) {
       
     for(var i=0 ; i<9;i++){
       
       console.log(b.items[i].snippet.title)
-      $("#trending-music").append(`<div class="col-md-4 mt-2">
-    <div class="card pb-3 " style="width:100%;height:100%">
-    <iframe class="iframe-size" src="https://www.youtube.com/embed/${b.items[i].snippet.resourceId.videoId}",allow="autoplay encrypted-media" frameborder='0' allowFullScreen width="100%" height="200px"></iframe><div class="card-body pb-2 "style="background:hotpink">
-    <h4 class="card-title  text-white"> Title:-${b.items[i].snippet.title}</h4><center>
-   <a href='http://api.youtube6download.top/fetch/link.php?i=${b.items[i].snippet.resourceId.videoId}' class="btn  pt-3 ml-2 download-buttons" target="blank"><i class="fa fa-download pr-1"></i> Download Mp3</a>
-    <a href="https://lolyoutube.com/download/mp4/${b.items[i].snippet.resourceId.videoId}/${Date.now()}"class="btn pt-3 ml-2 download-buttons" target="blank"><i class="fa fa-download  pr-1"></i> Download Mp4</a></center></div></div> </div>`)
+      $("#trending-music").append(`<div class="col-md-4 mt-5">
+    <div class="card pb-5 " style="width:100%;height:100%">
+    <iframe class="iframe-size mt-1" src="https://www.youtube.com/embed/${b.items[i].id.videoId}",allow="autoplay encrypted-media" frameborder='0' allowFullScreen width="100%" height="200px"></iframe><div class="card-body "style="background:hotpink">
+    <h4 class="card-title  text-white"> Title:-${b.items[i].snippet.channelTitle}</h4><center>
+   <a href='http://api.youtube6download.top/fetch/link.php?i=${b.items[i].id.videoId}' class="btn  pt-3 ml-2 download-buttons" target="blank"><i class="fa fa-download pr-1"></i> Download Mp3</a>
+    <a href="https://lolyoutube.com/download/mp4/${b.items[i].id.videoId}/${Date.now()}"class="btn pt-3 ml-2 download-buttons" target="blank"><i class="fa fa-download  pr-1"></i> Download Mp4</a></center></div></div> </div>`)
         
       
     }
